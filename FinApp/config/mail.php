667f500a4b3e7a8b81eb4ec0584097bdd5317835
Heriberto Sosa <sosaheriberto2001@gmail.com>
2019-10-30 16:18:30 -0400

    create auth system, spanish location

--- FinApp/config/mail.php
+++ FinApp/config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', '127.0.0.1'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 1025),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@dominio.com'),
+        'name' => env('MAIL_FROM_NAME', 'test'),
     ],
 
     /*
